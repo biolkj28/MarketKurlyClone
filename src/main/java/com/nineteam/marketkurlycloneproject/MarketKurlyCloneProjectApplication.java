25d7a9a9d79de7477bba7a69a5fdd96de84010a9
Lee Jeong Chan <biolkj28@gmail.com>
2022-06-18 15:20:08 +0900

    Springboot Project Initialize

---
+++ src/main/java/com/nineteam/marketkurlycloneproject/MarketKurlyCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nineteam.marketkurlycloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketKurlyCloneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketKurlyCloneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nineteam/marketkurlycloneproject/MarketKurlyCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineteam.marketkurlycloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketKurlyCloneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
