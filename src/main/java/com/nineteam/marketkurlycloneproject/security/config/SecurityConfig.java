4ccaf12873e12f329938e1816046f8ecac4cd289
kootner <refomto@naver.com>
2022-06-20 18:58:05 +0900

    회원 가입 및 로그인 기능 설정

---
+++ src/main/java/com/nineteam/marketkurlycloneproject/security/config/SecurityConfig.java
@@ -0,0 +1,88 @@
+package com.nineteam.marketkurlycloneproject.security.config;
+
+import com.nineteam.marketkurlycloneproject.security.jwt.JwtAccessDeniedHandler;
+import com.nineteam.marketkurlycloneproject.security.jwt.JwtAuthenticationEntryPoint;
+import com.nineteam.marketkurlycloneproject.security.jwt.JwtSecurityConfig;
+import com.nineteam.marketkurlycloneproject.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity  // 기본적인 웹보안 활성화
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    // 비밀번호 인코딩
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().configurationSource(corsConfigurationSource());
+        http
+                .csrf().disable()   // 토큰 방식 사용하기 때문에 csrf 설정 비활성
+
+                .exceptionHandling()    // exceptionHandling 할때 직접 만든 걸로 변경
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                .and()  // H2-console 을 위한 설정
+                .headers().frameOptions().disable()
+
+                .and()  // 세션 사용하지 않기 때문에 무상태로 변경
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()  // 토큰 없는 상태에서 진입 허가
+                .authorizeRequests()
+                .antMatchers("/user/test").hasRole("USER")
+                .antMatchers("/", "/user/**", "/css/**", "/images/**",
+                        "/js/**", "/h2-console/**","/favicon.ico").permitAll()
+                .anyRequest().authenticated()
+
+                .and()   // JwtFilter 를 등록한 JwtSecurityConfig 적용
+                .apply(new JwtSecurityConfig(tokenProvider));
+
+        return http.build();
+    }
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addExposedHeader("Authorization");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+}
