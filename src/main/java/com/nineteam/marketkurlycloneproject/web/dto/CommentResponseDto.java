79966eada73d4233c25ac9f47f9a0991324b6d90
codlg <lgj9889@gmail.com>
2022-06-22 03:59:39 +0900

    comment 수정

--- src/main/java/com/nineteam/marketkurlycloneproject/web/dto/CommentResponseDto.java
+++ src/main/java/com/nineteam/marketkurlycloneproject/web/dto/CommentResponseDto.java
@@ -1,6 +1,7 @@
 package com.nineteam.marketkurlycloneproject.web.dto;
 
 import com.nineteam.marketkurlycloneproject.domain.model.Comment;
+import com.nineteam.marketkurlycloneproject.domain.model.Products;
 import com.nineteam.marketkurlycloneproject.security.model.User;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,16 +11,18 @@ import lombok.Setter;
 public class CommentResponseDto {
 
     private Long commentId;
+    private Products products;
+    private User user;
     private String title;
-    private String loginId;
     private String comment;
     private String createdAt;
     private String commentImg;
 
     public CommentResponseDto(Comment comment) {
         this.commentId = comment.getId();
+        this.user = comment.getUser();
+        this.products = comment.getProducts();
         this.title = comment.getTitle();
-        this.loginId = comment.getUser().getLoginId();
         this.comment = comment.getComment();
         this.createdAt = comment.getCreatedAt();
         this.commentImg = comment.getCommentImg();
